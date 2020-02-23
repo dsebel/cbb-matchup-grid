9f168edde2509c0a69f67b6090a8fdc9f9dbcb21
Drew Ebelhar <drewebelhar@gmail.com>
2020-02-23 12:20:33 -0800

    Updates

---
+++ precache-manifest.3e125680b2da892a537470ea800cf2b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a912c8ea3cd4095cf5212b98e32de2",
+    "url": "/cbb-matchup-grid/index.html"
+  },
+  {
+    "revision": "9b3fda41e138940533c8",
+    "url": "/cbb-matchup-grid/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a52dfc0d96552224d0af",
+    "url": "/cbb-matchup-grid/static/js/2.8a1c8af4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cbb-matchup-grid/static/js/2.8a1c8af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3fda41e138940533c8",
+    "url": "/cbb-matchup-grid/static/js/main.2cc1748e.chunk.js"
+  },
+  {
+    "revision": "f1878ed90266669416bd",
+    "url": "/cbb-matchup-grid/static/js/runtime-main.4438856e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cbb-matchup-grid/static/media/logo.5d5d9eef.svg"
+  }
+]);
