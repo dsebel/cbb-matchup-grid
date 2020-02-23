519c6bf4355735466aa1157e858d9bbd3437e9a1
Drew Ebelhar <drewebelhar@gmail.com>
2020-02-23 14:03:42 -0800

    Add tournament seed dropdowns

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Papa from 'papaparse';
 
+import SeedInput from './SeedInput';
 import './App.css';
 import gameData from '../data/Big_Dance_CSV.csv';
 
@@ -9,8 +10,19 @@ class App extends Component {
     super(props);
 
     this.state = {
+      // All games, grouped by sorted seed matchups.
       games: {},
+
+      // Filtered games, all games currently selected.
+      filteredGames: [],
+
+      // Selected seeds.
+      leftSeed: 1,
+      rightSeed: 1,
     };
+
+    this.handleLeftSeedUpdate = this.handleLeftSeedUpdate.bind(this);
+    this.handleRightSeedUpdate = this.handleRightSeedUpdate.bind(this);
   }
 
   componentDidMount() {
@@ -24,13 +36,67 @@ class App extends Component {
       skipEmptyLines: true,
       dynamicTyping: true,
       complete: result => {
-        console.log(result);
+        // Load games into a JS object, using the sorted seed values as keys
+        // for easier lookup.
+        let games = {};
+        result.data.forEach(row => {
+          const seedKey =
+            row['Seed A'] <= row['Seed B']
+              ? `${row['Seed A']}-${row['Seed B']}`
+              : `${row['Seed B']}-${row['Seed A']}`;
+          games[seedKey] ? games[seedKey].push(row) : (games[seedKey] = [row]);
+        });
+        this.setState({ games }, this.filterGames);
       },
     });
   }
 
+  handleLeftSeedUpdate(event) {
+    event.preventDefault();
+    this.setState(
+      { leftSeed: parseInt(event.target.value, 10) },
+      this.filterGames
+    );
+  }
+
+  handleRightSeedUpdate(event) {
+    event.preventDefault();
+    this.setState(
+      { rightSeed: parseInt(event.target.value, 10) },
+      this.filterGames
+    );
+  }
+
+  filterGames() {
+    const seedKey =
+      this.state.leftSeed <= this.state.rightSeed
+        ? `${this.state.leftSeed}-${this.state.rightSeed}`
+        : `${this.state.rightSeed}-${this.state.leftSeed}`;
+    let filteredGames = [];
+    if (this.state.games[seedKey]) {
+      filteredGames = this.state.games[seedKey];
+    }
+    this.setState({ filteredGames });
+  }
+
   render() {
-    return <div>TODO</div>;
+    return (
+      <div>
+        <SeedInput
+          leftSeed={this.state.leftSeed}
+          onLeftSeedDropdownChange={this.handleLeftSeedUpdate}
+          rightSeed={this.state.rightSeed}
+          onRightSeedDropdownChange={this.handleRightSeedUpdate}
+        ></SeedInput>
+
+        {this.state.filteredGames.map(fg => (
+          <div key={`${fg['Year']}-${fg['Team A']}-${fg['Team B']}`}>
+            {fg['Year']}: {fg['Seed A']} {fg['Team A']} vs {fg['Seed B']}{' '}
+            {fg['Team B']}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
---
+++ src/components/SeedInput.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+function SeedInput(props) {
+  return (
+    <Container>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <FormControl>
+            <InputLabel id="seed-1-label">Seed</InputLabel>
+            <Select
+              labelId="seed-1"
+              className="seed-dropdown"
+              value={props.leftSeed}
+              onChange={props.onLeftSeedDropdownChange}
+            >
+              <MenuItem value="1">1</MenuItem>
+              <MenuItem value="2">2</MenuItem>
+              <MenuItem value="3">3</MenuItem>
+              <MenuItem value="4">4</MenuItem>
+              <MenuItem value="5">5</MenuItem>
+              <MenuItem value="6">6</MenuItem>
+              <MenuItem value="7">7</MenuItem>
+              <MenuItem value="8">8</MenuItem>
+              <MenuItem value="9">9</MenuItem>
+              <MenuItem value="10">10</MenuItem>
+              <MenuItem value="11">11</MenuItem>
+              <MenuItem value="12">12</MenuItem>
+              <MenuItem value="13">13</MenuItem>
+              <MenuItem value="14">14</MenuItem>
+              <MenuItem value="15">15</MenuItem>
+              <MenuItem value="16">16</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item>vs.</Grid>
+        <Grid item>
+          <FormControl>
+            <InputLabel id="seed-2-label">Seed</InputLabel>
+            <Select
+              labelId="seed-2"
+              className="seed-dropdown"
+              value={props.rightSeed}
+              onChange={props.onRightSeedDropdownChange}
+            >
+              <MenuItem value="1">1</MenuItem>
+              <MenuItem value="2">2</MenuItem>
+              <MenuItem value="3">3</MenuItem>
+              <MenuItem value="4">4</MenuItem>
+              <MenuItem value="5">5</MenuItem>
+              <MenuItem value="6">6</MenuItem>
+              <MenuItem value="7">7</MenuItem>
+              <MenuItem value="8">8</MenuItem>
+              <MenuItem value="9">9</MenuItem>
+              <MenuItem value="10">10</MenuItem>
+              <MenuItem value="11">11</MenuItem>
+              <MenuItem value="12">12</MenuItem>
+              <MenuItem value="13">13</MenuItem>
+              <MenuItem value="14">14</MenuItem>
+              <MenuItem value="15">15</MenuItem>
+              <MenuItem value="16">16</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+SeedInput.propTypes = {
+  leftSeed: PropTypes.number,
+  onLeftSeedDropdownChange: PropTypes.func,
+  rightSeed: PropTypes.number,
+  onRightSeedDropdownChange: PropTypes.func,
+};
+
+export default SeedInput;
