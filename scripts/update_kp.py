7ab68aee1afcfca81559dc52cb9e7448df28a477
Drew Ebelhar <drewebelhar@gmail.com>
2020-03-01 12:15:50 -0800

    Add seed generator to spoof seeds for 2020
    Temporary until real bracket and seeds are released.

--- scripts/update_kp.py
+++ scripts/update_kp.py
@@ -5,6 +5,7 @@ on a one-time or weekly basis. Respect the robots.txt!
 FIXME: Will require an update when seeds are released & added to team names.
 """
 
+import math
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -27,6 +28,7 @@ if __name__ == '__main__':
     # Extract data from table rows.
     kp_rows = soup.find('table', {'id': 'ratings-table'}).find_all('tr')
     kp_ratings = []
+    seed_gen = 0  # temporary until seeds are released
     for kp_row in kp_rows:
         kp_cols = kp_row.find_all(['td', 'th'])
         if len(kp_cols) == 8 or len(kp_cols) == 13:
@@ -39,7 +41,7 @@ if __name__ == '__main__':
             2020,
             int(kp_cols[0].text),
             team_name,
-            -1,  # seed
+            int(math.floor(seed_gen / 4) + 1) if seed_gen < 64 else -1,  # seed
             kp_cols[2].text,
             kp_cols[3].text,
             float(kp_cols[4].text),  # adj em
@@ -60,6 +62,7 @@ if __name__ == '__main__':
             float(kp_cols[19].text),  # NCSoS
             int(kp_cols[20].text),
         ])
+        seed_gen += 1
 
     # Write to file.
     df = pd.DataFrame().from_records(kp_ratings)
