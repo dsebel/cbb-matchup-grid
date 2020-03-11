e6ecc9f7e81d007aa7966b3c54bc36c1ac48ea56
Drew Ebelhar <drewebelhar@gmail.com>
2020-03-10 18:23:07 -0700

    Put chart filters in an accordion

--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -1,38 +1,57 @@
 import React from 'react';
-import { Checkbox, Form, Grid, Header, Icon, Popup } from 'semantic-ui-react';
+import {
+  Accordion,
+  Checkbox,
+  Form,
+  Grid,
+  Icon,
+  Popup,
+} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 function FilterInput(props) {
   return (
     <Grid.Row>
       <Grid.Column>
-        <Header as="h3">Filters</Header>
-        <Grid columns={3}>
-          <Grid.Column>
-            <Checkbox
-              label="Show Potential Matchups"
-              defaultChecked={props.showPotentialMatchups}
-              onChange={props.onPotentialMatchupsCheckboxChange}
-            />{' '}
-            <Popup
-              trigger={<Icon name="info circle" color="blue" />}
-              content="Show teams with the selected seeds for the current year."
-              style={{ left: '-12px' }}
-            />
-          </Grid.Column>
-          <Grid.Column as={Form}>
-            <Form.Input
-              label={`Starting Year: ${props.startingYear}`}
-              min={2010}
-              max={2020}
-              name="min-year"
-              onChange={props.onStartingYearChange}
-              step={1}
-              type="range"
-              value={props.startingYear}
-            ></Form.Input>
-          </Grid.Column>
-        </Grid>
+        <Accordion
+          as={Form.Field}
+          panels={[
+            {
+              key: 'potential',
+              title: 'Filters',
+              content: {
+                content: (
+                  <Grid columns={2}>
+                    <Grid.Column>
+                      <Checkbox
+                        label="Show Potential Matchups"
+                        defaultChecked={props.showPotentialMatchups}
+                        onChange={props.onPotentialMatchupsCheckboxChange}
+                      />{' '}
+                      <Popup
+                        trigger={<Icon name="info circle" color="blue" />}
+                        content="Show teams with the selected seeds for the current year."
+                        style={{ left: '-12px' }}
+                      />
+                    </Grid.Column>
+                    <Grid.Column as={Form}>
+                      <Form.Input
+                        label={`Starting Year: ${props.startingYear}`}
+                        min={2010}
+                        max={2020}
+                        name="starting-year"
+                        onChange={props.onStartingYearChange}
+                        step={1}
+                        type="range"
+                        value={props.startingYear}
+                      ></Form.Input>
+                    </Grid.Column>
+                  </Grid>
+                ),
+              },
+            },
+          ]}
+        ></Accordion>
       </Grid.Column>
     </Grid.Row>
   );
